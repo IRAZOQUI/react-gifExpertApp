88c791d60e0738fbb958e1fb68e7908c0d64c454
Erick Irazoqui <halo_iraz@hotmail.com>
2020-10-25 09:30:18 -0700

    docs

---
+++ docs/precache-manifest.1df0642be203cdf756d06dd8a8adc9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566bfef6ce0a2db2f68fa25778406d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1959d9bbd8781c7a8b",
+    "url": "/static/css/main.581bdcca.chunk.css"
+  },
+  {
+    "revision": "36d498c5d143d506d8f1",
+    "url": "/static/js/2.0cc3d387.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.0cc3d387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1959d9bbd8781c7a8b",
+    "url": "/static/js/main.3a40d563.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
